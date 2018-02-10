88e9e48f0317a6c53d9bdb8689da33643dc23638
Yurii Acronym <35079110+farfarawaaay@users.noreply.github.com>
2018-02-10 22:12:12 +0200

    add smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,19 @@ $(function(){
       $spinner.fadeOut();
       $preloader.delay(350).fadeOut('slow');
   });
+
   $('.burger').click(function() {
     $('.menu-collapse').toggleClass('d-none');
     $('.menu-collapse').toggleClass('menu__active');
   });
+
+  $("a.feature__btn").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+         duration: 600,
+         easing: "swing"
+      });
+      return false;
+   });
 });
